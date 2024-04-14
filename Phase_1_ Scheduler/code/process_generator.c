97acd476d4d4f8e5a71adbe0e9c4d38a6c4803e2
nourabouzeid <noody081@gmail.com>
2024-04-14 14:11:46 +0200

    nour commit

--- Phase_1_ Scheduler/code/process_generator.c
+++ Phase_1_ Scheduler/code/process_generator.c
@@ -1,5 +1,5 @@
 #include "headers.h"
-
+#define NUMOFPROC 3
 void clearResources(int);
 
 int main(int argc, char * argv[])
@@ -7,7 +7,42 @@ int main(int argc, char * argv[])
     signal(SIGINT, clearResources);
     // TODO Initialization
     // 1. Read the input files.
+    FILE *file;
+    file = fopen("processes.txt", "r");
+    int id[NUMOFPROC];
+    int arrival[NUMOFPROC];
+    int runtime[NUMOFPROC];
+    int priority[NUMOFPROC];
+    char data[100];
+    if(file) 
+    {
+        int i=0;
+        while(fgets(data,100,file))
+        { 
+            if(data[0]=='#')
+                continue;
+            sscanf(data,"%d %d %d %d",&id[i],&arrival[i],&runtime[i],&priority[i]);
+            printf("%d    %d     %d     %d\n",id[i],arrival[i],runtime[i],priority[i]);
+            i++;
+        }
+
+    } 
+    else 
+    {
+        printf("Not able to open the file.");
+    }
+    fclose(file); 
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
+    printf("Choose a scheduling algorithm\n1)RR\n2)HPF\n3)SRTN\n");
+    int algo,quantum;
+    scanf("%d",&algo);
+    if(algo==1)
+    {
+        printf("Enter Quantum for RR\n");
+        scanf("%d",&quantum);
+    }
+
+
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock
     initClk();
