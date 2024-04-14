0044403b95df92e0fe5cc7acb56570d90f4980f2
salma <salmamohed207@gmail.com>
2024-04-14 17:37:01 +0200

    small

--- Phase_1_ Scheduler/code/process.c
+++ Phase_1_ Scheduler/code/process.c
@@ -7,12 +7,30 @@ int remainingtime;
 int main(int agrc, char * argv[])
 {
     initClk();
+     key_t keymem = ftok("keyfile", 's');
+    if (keymem == -1)
+    {
+        perror("Error in ftok memory");
+        exit(-1);
+    }
+
+    int shmid = shmget(keymem, 4, IPC_CREAT | 0644);
+
+    if (shmid == -1)
+    {
+        perror("Error in create");
+        exit(-1);
+    } 
+    void *shmaddr = shmat(shmid, (void *)0, 0);
     
     //TODO it needs to get the remaining time from somewhere
-    //remainingtime = ??;
+    int remainingtime = argv[1];
     while (remainingtime > 0)
     {
-        // remainingtime = ??;
+        
+        strcpy((char *)shmaddr, itoa(remainingtime--));
+        //strcpy((char *)shmaddr, itoa(remainingtime--));
+
     }
     
     destroyClk(false);
--- Phase_1_ Scheduler/code/process_generator.c
+++ Phase_1_ Scheduler/code/process_generator.c
@@ -1,46 +1,19 @@
 #include "headers.h"
 #define NUMOFPROC 3
+#include "PCB.h"
+#include <string.h>
+#include "PriorityQueue.h"
 void clearResources(int);
-
+int algo;
+int quantuam;
+struct PCB currentprocess;
+PriorityQueue * ready;
 int main(int argc, char * argv[])
 {
     signal(SIGINT, clearResources);
     // TODO Initialization
     // 1. Read the input files.
-    FILE *file;
-    file = fopen("processes.txt", "r");
-    int id[NUMOFPROC];
-    int arrival[NUMOFPROC];
-    int runtime[NUMOFPROC];
-    int priority[NUMOFPROC];
-    char data[100];
-    if(file) 
-    {
-        int i=0;
-        while(fgets(data,100,file))
-        { 
-            if(data[0]=='#')
-                continue;
-            sscanf(data,"%d %d %d %d",&id[i],&arrival[i],&runtime[i],&priority[i]);
-            printf("%d    %d     %d     %d\n",id[i],arrival[i],runtime[i],priority[i]);
-            i++;
-        }
-
-    } 
-    else 
-    {
-        printf("Not able to open the file.");
-    }
-    fclose(file); 
-    // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
-    printf("Choose a scheduling algorithm\n1)RR\n2)HPF\n3)SRTN\n");
-    int algo,quantum;
-    scanf("%d",&algo);
-    if(algo==1)
-    {
-        printf("Enter Quantum for RR\n");
-        scanf("%d",&quantum);
-    }
+    readFile();
 
 
     // 3. Initiate and create the scheduler and clock processes.
@@ -55,7 +28,52 @@ int main(int argc, char * argv[])
     // 7. Clear clock resources
     destroyClk(true);
 }
+void chooseschuldler()
+{
 
+}
+void readFile(char * filename)
+{
+    FILE *file;
+    file = fopen(filename, "r");
+    int id[NUMOFPROC];
+    int arrival[NUMOFPROC];
+    int runtime[NUMOFPROC];
+    int priority[NUMOFPROC];
+    char data[100];
+    if(file) 
+    {
+        int i=0;
+        while(fgets(data,100,file))
+        { 
+            if(data[0]=='#')
+                continue;
+            sscanf(data,"%d %d %d %d",&id[i],&arrival[i],&runtime[i],&priority[i]);
+            currentprocess.arrivaltime=arrival[i];
+            currentprocess.runningtime=runtime[i];
+            currentprocess.priority=priority[i];
+            currentprocess.pid=id[i];
+            enqueue(ready,currentprocess,currentprocess.arrivaltime);
+            printf("%d    %d     %d     %d\n",id[i],arrival[i],runtime[i],priority[i]);
+            i++;
+        }
+
+    } 
+    else 
+    {
+        printf("Not able to open the file.");
+    }
+    fclose(file); 
+    // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
+    //printf("Choose a scheduling algorithm\n1)RR\n2)HPF\n3)SRTN\n");
+    /*int algo,quantum;
+    scanf("%d",&algo);
+    if(algo==1)
+    {
+        printf("Enter Quantum for RR\n");
+        scanf("%d",&quantum);
+    }*/
+}
 void clearResources(int signum)
 {
     //TODO Clears all resources in case of interruption
